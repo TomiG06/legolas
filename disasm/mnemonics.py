3e833260ea9270e8b35fda32ed3069991027617e
TomiG06 <thomasgkountas@gmail.com>
2022-06-16 21:57:41 +0300

    opcodes to 0x6B

--- disasm/mnemonics.py
+++ disasm/mnemonics.py
@@ -1,4 +1,5 @@
 #Instruction Prefixes
+
 OP_SIZE     = 0x66
 ADDR_SIZE   = 0x67
 SEG_CS      = 0x2E
@@ -19,7 +20,7 @@ sgmnt = dict(zip(sgmnt_ovrd, sgmnt_name))
 pfx = [OP_SIZE, ADDR_SIZE, *sgmnt_ovrd, REPEAT, LOCK]
 
 #Opcodes
-opcodes = {}
+
 #1 byte
 
 ADD_rm8_r8                  = 0x00
@@ -54,6 +55,46 @@ SBB_al_imm8                 = 0x1C
 SBB_eax_imm1632             = 0x1D
 PUSH_ds                     = 0x1E
 POP_ds                      = 0x1F
+AND_rm8_r8                  = 0x20
+AND_rm1632_r1632            = 0x21
+AND_r8_rm8                  = 0x22
+AND_r1632_rm1632            = 0x23
+AND_al_imm8                 = 0x24
+AND_eax_imm1632             = 0x25
+DAA                         = 0x27
+SUB_rm8_r8                  = 0x28
+SUB_rm1632_r1632            = 0x29
+SUB_r8_rm8                  = 0x2A
+SUB_r1632_rm1632            = 0x2B
+SUB_al_imm8                 = 0x2C
+SUB_eax_imm1632             = 0x2D
+DAS                         = 0x2F
+XOR_rm8_r8                  = 0x30
+XOR_rm1632_r1632            = 0x31
+XOR_r8_rm8                  = 0x32
+XOR_r1632_rm1632            = 0x33
+XOR_al_imm8                 = 0x34
+XOR_eax_imm1632             = 0x35
+AAA                         = 0x37
+CMP_rm8_r8                  = 0x38
+CMP_rm1632_r1632            = 0x39
+CMP_r8_rm8                  = 0x3A
+CMP_r1632_rm1632            = 0x3B
+CMP_al_imm8                 = 0x3C
+CMP_eax_imm1632             = 0x3D
+AAS                         = 0x3F
+INC_r1632                   = 0x40
+DEC_r1632                   = 0x48
+PUSH_r1632                  = 0x50
+POP_r1632                   = 0x58
+PUSHA                       = 0x60
+POPA                        = 0x61
+BOUND_r1632_m1632           = 0x62
+ARPL_rm16_r16               = 0x63
+PUSH_imm1632                = 0x68
+IMUL_r1632_rm1632_imm1632   = 0x69
+PUSH_imm8                   = 0x6A
+IMUL_r1632_rm1632_imm8      = 0x6B
 
 
 
