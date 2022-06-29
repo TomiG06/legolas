3956857d037d231a5c6ec9a835385c73a3fdd571
TomiG06 <thomasgkountas@gmail.com>
2022-06-29 09:58:38 +0300

    moved registers.h in opcode.h

--- disasm/opcodes.h
+++ disasm/opcodes.h
@@ -426,4 +426,33 @@
 #define JMP_rm1632                  0xFF
 #define PUSH_rm1632                 0xFF
 
+
+enum {
+    eax,
+    ecx,
+    edx,
+    ebx,
+    esp,
+    ebp,
+    esi,
+    edi,
+
+    reg_c
+};
+
+
+enum {
+    al,
+    cl,
+    dl,
+    bl,
+    ah,
+    ch,
+    dh,
+    bh
+};
+
+const char reg32[reg_c][3] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
+const char reg8[reg_c][2] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+
 #endif
