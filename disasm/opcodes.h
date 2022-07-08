253a9e0df9ba6ba5ae3eade7076871a29d163e48
TomiG06 <thomasgkountas@gmail.com>
2022-07-08 13:56:46 +0300

    added modrm16 combination truth table

--- disasm/opcodes.h
+++ disasm/opcodes.h
@@ -455,4 +455,6 @@ enum {
 const char reg32[reg_c][4] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
 const char reg8[reg_c][3] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 
+const char rm16[][6] = {"bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"};
+
 #endif
