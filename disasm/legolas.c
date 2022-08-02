9c163a646c20d68270cf54cb92b8dc1fb2359f46
TomiG06 <thomasgkountas@gmail.com>
2022-08-02 12:11:13 +0300

    simplified ELF header reading

--- disasm/legolas.c
+++ disasm/legolas.c
@@ -7,25 +7,6 @@
 #include "helf.h"
 #include "disasm.h"
 
-Elf32_Ehdr* elf_header(FILE* f) {
-    static Elf32_Ehdr hdr;
-    fread(hdr.e_ident, sizeof(char), EI_NIDENT, f);
-    fread(&hdr.e_type, SHalf, 1, f);
-    fread(&hdr.e_machine, SHalf, 1, f);
-    fread(&hdr.e_version, SWord, 1, f); 
-    fread(&hdr.e_entry, SAddr, 1, f); 
-    fread(&hdr.e_phoff, SOff, 1, f); 
-    fread(&hdr.e_shoff, SOff, 1, f);
-    fread(&hdr.e_flags, SWord, 1, f); 
-    fread(&hdr.e_ehsize, SHalf, 1, f); 
-    fread(&hdr.e_phentsize, SHalf, 1, f); 
-    fread(&hdr.e_phnum, SHalf, 1, f);
-    fread(&hdr.e_shentsize, SHalf, 1, f);
-    fread(&hdr.e_shnum, SHalf, 1, f);
-    fread(&hdr.e_shstrndx, SHalf, 1, f);
-    return &hdr;
-}
-
 static const char magic_num[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
 
 int main(int argc, char* argv[]) {
@@ -41,36 +22,37 @@ int main(int argc, char* argv[]) {
 
     FILE* f = fopen(argv[1], "rb");
 
-    Elf32_Ehdr* hdr = elf_header(f);
+    Elf32_Ehdr hdr;
 
+    fread(&hdr, sizeof(Elf32_Ehdr), 1, f);
 
     //Check if file is ELF
-    if(strncmp(magic_num, hdr->e_ident, sizeof(magic_num))) {
+    if(strncmp(magic_num, hdr.e_ident, sizeof(magic_num))) {
         printf("File '%s' is not ELF\n", argv[1]);
         return 1;
     }
 
     //Check if ELF file's target ABI is System V (Linux outputs the same, at least Ubuntu does)
-    if(hdr->e_ident[EI_OSABI]) {
+    if(hdr.e_ident[EI_OSABI]) {
         printf("File's target ABI is not System V\n");
         return 1;
     }
 
     //Check if ELF file's target is i386
-    if(hdr->e_machine != EM_386) {
+    if(hdr.e_machine != EM_386) {
         printf("File '%s' is not compatible with i386\n", argv[1]);
         return 1;
     }
 
     //Check if ELF file is in 32-bit format
-    if(hdr->e_ident[EI_CLASS] != ELFCLASS32) {
+    if(hdr.e_ident[EI_CLASS] != ELFCLASS32) {
         printf("File '%s' is not 32-bit\n", argv[1]);
         return 1;
     }
 
-    char** sh = extract_sheaders(hdr, f);
-    int16_t text_index = shindexof(".text", sh, hdr);
-    int16_t symtab_index = shindexof(".symtab", sh, hdr);
+    char** sh = extract_sheaders(&hdr, f);
+    int16_t text_index = shindexof(".text", sh, &hdr);
+    int16_t symtab_index = shindexof(".symtab", sh, &hdr);
 
     if(text_index < 0) {
         printf(".text section not found\n");
